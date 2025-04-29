c5d3e64a555255a749adeeb239c79b81f78bcc87
TomoXiang710 <celticwoman710@gmail.com>
2025-04-29 16:23:56 +0900

    タスクの追加機能を実装

---
+++ src/index.mjs
@@ -0,0 +1,44 @@
+import "./style.css";
+
+const OnClickAdd = () => {
+    // テキストボックスの値を取得し、初期化する
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value;
+
+    // li生成
+    const li = document.createElement("li");
+
+    // div生成
+    const div = document.createElement("div");
+    div.className = "list-row";
+
+    // p生成
+    const p = document.createElement("p");
+    p.className = "todo-item";
+    p.innerText = inputText;
+
+    // button（完了）タグを生成
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "完了";
+    completeButton.addEventListener("click", () => {
+        alert("完了");
+    });
+
+    // button（削除）タグを生成
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "削除";
+    deleteButton.addEventListener("click", () => {
+        alert("削除");
+    });
+
+    // liタグの子要素に各要素を設定
+    li.appendChild(div);
+    div.appendChild(p);
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
+
+    // 未完了リストに追加
+    document.getElementById("incomplete-list").appendChild(li);
+}
+
+document.getElementById("add-button").addEventListener("click", OnClickAdd);
